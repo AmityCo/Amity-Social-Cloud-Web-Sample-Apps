f198676c3d4e3564d9434a3e6d09bbc7c2aede5b
Julien Barbay <julien.barbay@gmail.com>
2021-03-25 16:22:38 +0700

    Update api keys

--- legacy-sample-app/src/sdk-config.js
+++ legacy-sample-app/src/sdk-config.js
@@ -1,6 +1,6 @@
 export default {
   // please place your API key here
-  SAMPLE_APP_KEY: 'b3bee858328ef4344a308e4a5a091688d05fdee2be353a2b',
+  SAMPLE_APP_KEY: 'b3bee90c39d9a5644831d84e5a0d1688d100ddebef3c6e78',
   DEFAULT_USER: {
     USER_ID: 'legacy-sample-app',
     DISPLAY_NAME: 'legacy-sample-app',
