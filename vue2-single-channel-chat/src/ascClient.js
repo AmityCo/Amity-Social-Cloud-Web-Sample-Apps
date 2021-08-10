bb9ab909a0e091745a5e5d0db8c7141eb260ff8d
touchaponk <touchaponk@gmail.com>
2021-08-10 16:25:50 +0700

    updated SDK to 5.4.0 + added region endpoint configuration

--- vue2-single-channel-chat/src/ascClient.js
+++ vue2-single-channel-chat/src/ascClient.js
@@ -1,8 +1,8 @@
-import ASCClient from "@amityco/js-sdk";
+import ASCClient, { ConnectionStatus, ApiEndpoint } from "@amityco/js-sdk";
 
 // Amity sample-app apiKey
 const apiKey = "b3bee90c39d9a5644831d84e5a0d1688d100ddebef3c6e78";
-export const client = ASCClient.create({ apiKey });
+export const client = ASCClient.create({ apiKey, apiEndpoint: ApiEndpoint.SG });
 
 export const isConnected = () => {
   return client.isConnected;
